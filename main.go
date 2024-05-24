f6a0d55b5b5c02aff6aa9fd2f5839c6e2ec49792
abdullatif033 <cra90091@gmail.com>
2024-05-24 12:11:14 +0300

    changes

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +16,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("arailym"))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Display the selected note ID @d...", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {

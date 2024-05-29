4704f4f60ec7e861244ba3fc618de777e238ae2c
abdullatif033 <cra90091@gmail.com>
2024-05-29 16:18:51 +0500

    Snippetbox

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,12 +13,25 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-}
-	err=ts.Execute(w,nil)
-		if err !nil {
-			log.Println(err.Error())
-		}
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
 
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
 func showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
